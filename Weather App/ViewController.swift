f74eeeee94ce5bf5c44f97c60621c4124c5cf59b
jeniferformagi <31970649+jeniferformagi@users.noreply.github.com>
2023-11-05 21:43:53 -0300

    setup do view code
    Estruturado a ViewController, adicionado as constraints em tela para visualizar o layout de forma a entender como as posições funcionam.

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,10 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
-    override func viewDidLoad() {
+    override func viewDidLoad() { 
+        //executado sempre que a view é carregada, uma só vez
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) { 
+        // quando a tela é carregada
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) { 
+        // antes da tela ser carregada
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) { 
+        // depois que a tela é carregada
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) { 
+        // quando a tela vai desaparecer ou desapareceu
+        super.viewDidDisappear(animated)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .red
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
     }
 
 
